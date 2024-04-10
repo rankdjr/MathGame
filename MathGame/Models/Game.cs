95615d48b7834177528392394699be9448f09ad6
Douglas Rank <Douglas Rank@DESKTOP-9LJDMPS>
2024-04-09 18:04:04 -0700

    Add project files.

---
+++ MathGame/Models/Game.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.AccessControl;
+
+namespace MathGame.Models;
+
+internal class Game
+{
+    public DateTime Date { get; set; }
+    public int Score { get; set; }
+    public GameType GameType { get; set; } // This already exists and fits perfectly
+    public DifficultyLevel DifficultyLevel { get; set; } // Continue using this to dictate the difficulty across the game
+}
---
+++ MathGame/Models/Question.cs
@@ -0,0 +1,10 @@
+﻿namespace MathGame.Models;
+
+internal class Question
+{
+    public required int FirstNumber { get; set; }
+    public required int SecondNumber { get; set; }
+    public required string OperationSymbol { get; set; }
+    public required int CorrectAnswer { get; set; }
+
+}
